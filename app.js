5676abae689426768ceb3fd1cdeaf7b53b191245
Bheeshamlal <bheeshamlal.bscssef20@iba-suk.edu.pk>
2024-11-06 12:57:14 +0500

    todo app

--- app.js
+++ app.js
@@ -6,6 +6,30 @@ let input = document.querySelector("input");
 btn.addEventListener("click", function(){
     let li = document.createElement("li");
     li.innerText = input.value;
+
+    let delBtn = document.createElement("button");
+    delBtn.innerText = "delete";
+    delBtn.classList.add("delete");
+    li.appendChild(delBtn);
+
     ul.appendChild(li);
     input.value = "";
-})
+
+
+    let delBtns = document.querySelectorAll(".delete");
+    for(delBtn of delBtns){
+        delBtn.addEventListener("click", function(){
+            let par = this.parentElement;
+            par.remove();
+        });
+    }
+});
+
+
+let delBtns = document.querySelectorAll(".delete");
+for(delBtn of delBtns){
+    delBtn.addEventListener("click", function(){
+        let par = this.parentElement;
+        par.remove();
+    });
+}
